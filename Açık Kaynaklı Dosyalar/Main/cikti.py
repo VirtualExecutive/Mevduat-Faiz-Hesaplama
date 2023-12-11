ea3b0032f7be08a611fc9ac231fa7ce1a59bd3e1
Yusuf Yazıcı <63202456+yusufYAZICI155@users.noreply.github.com>
2023-12-11 19:05:46 +0300

    Float değerler 0.2f olarak ayarlandı.

--- Açık Kaynaklı Dosyalar/Main/cikti.py
+++ Açık Kaynaklı Dosyalar/Main/cikti.py
@@ -4,4 +4,4 @@ class Cikti():
     zamanStr:str
 
     def AnlikIstatistikYazdir(zamanStr:str, gecenZaman:int, miktar:float,kar:float) -> None:
-        print(f"{gecenZaman}. {zamanStr} | {miktar} + {kar} = {miktar+kar}")
+        print(f"{gecenZaman}. {zamanStr} | {(miktar):0.2f} + {(kar):0.2f} = {(miktar+kar):0.2f}")
